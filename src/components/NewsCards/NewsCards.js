0a067d27f25feaa810b714d5f6b2b7772894c231
KitwanaSh <rodriguekitweze@gmail.com>
2023-09-12 21:09:33 +0000

    Fix: Update a property

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -19,17 +19,17 @@ const NewsCards = ({ articles }) => {
             <Grow in>
                 <Grid className={classes.container} container alignItems='stretch' spacing={3}>
                     {infoCards.map(infoCard => (
-                        <Grid xs={12} sm={6} md ={4} lg={3} className={classes.infoCard}>
+                        <Grid xs={12} sm={6} md={4} lg={3} className={classes.infoCard}>
                             <div className={classes.card} style={{ backgroundColor: infoCard.color}}>
                                 <Typography variant="h5">{infoCard.title}</Typography>
                                 {
                                     infoCard.info  
                                     ? (<Typography variant='h6'>
                                         <strong>
-                                            {infoCard.title.split(' ')[2]}
-                                            <br />
-                                            {infoCard.info}
+                                            {infoCard.title.split(' ')[2]}:
                                         </strong>
+                                        <br />
+                                        {infoCard.info}
                                     </Typography>) : null
                                 }
                                 <Typography variant='h6'>Try saying: <i>{infoCard.text}</i></Typography>
@@ -44,7 +44,7 @@ const NewsCards = ({ articles }) => {
     <Grow in>
         <Grid className={classes.container} container alignItems='stretch' spacing={3}>
             {articles.map((article, i) => (
-                <Grid item xs={12} sm={6} md={4} lg={4} style={{ display: 'flex' }} >
+                <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }} >
                     <NewsCard article={article} i={i} />
                 </Grid>
             ))}
