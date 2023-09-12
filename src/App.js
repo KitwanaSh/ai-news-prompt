3a2aefe57dba57d066e8932c18b91a75120c5265
KitwanaSh <rodriguekitweze@gmail.com>
2023-09-12 14:49:22 +0000

    Feat: Update the App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React, { useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import alanBtn from "@alan-ai/alan-sdk-web"
+import NewsCards from "./components/NewsCards/NewsCards";
 
 const alanKey = 'cd452736e273b971183dc4aef83d8af12e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+    const [newsArticles, setNewsArticles] = useState([]);
+
     useEffect(() => {
         alanBtn({
             key: alanKey,
             onCommand: ({ command, articles }) => {
                 if(command === 'newHeadlines') {
-                    console.log(articles)
+                    setNewsArticles(articles)
                 }
             }
         })
@@ -19,6 +22,7 @@ const App = () => {
     return (
         <div>
             <h1>Welcome to Alan AI, your article audio reader.</h1>
+            <NewsCards articles={newsArticles} />
         </div>
     )
 }
