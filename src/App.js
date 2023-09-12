c6ec5d3b0026af21a0a0d47480e1278b6397d8ad
KitwanaSh <rodriguekitweze@gmail.com>
2023-09-12 13:04:40 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react"
+import alanBtn from "@alan-ai/alan-sdk-web"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const alanKey = 'cd452736e273b971183dc4aef83d8af12e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App = () => {
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command, articles }) => {
+                if(command === 'newHeadlines') {
+                    console.log(articles)
+                }
+            }
+        })
+    }, [])
+
+
+    return (
+        <div>
+            <h1>Welcome to Alan AI, your article audio reader.</h1>
+        </div>
+    )
 }
 
-export default App;
+export default App
