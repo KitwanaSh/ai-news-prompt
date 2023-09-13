e1b9a5427885bf9e3a9d0494b77b930a3f25b1df
KitwanaSh <rodriguekitweze@gmail.com>
2023-09-13 10:56:59 +0000

    Feat: Add head image

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from "react"
 import alanBtn from "@alan-ai/alan-sdk-web"
 import NewsCards from "./components/NewsCards/NewsCards";
 
+import useStyles from "./styles.js"
+
 const alanKey = 'cd452736e273b971183dc4aef83d8af12e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
     const [newsArticles, setNewsArticles] = useState([]);
+    const classes = useStyles();
 
     useEffect(() => {
         alanBtn({
@@ -21,7 +24,9 @@ const App = () => {
 
     return (
         <div>
-            <h1>Welcome to Alan AI, your article audio reader.</h1>
+            <div className={classes.logoContainer}>
+                <img src="https://www.dits.center/images/picture13_1.jpg" className={classes.alanLogo} alt="Heading" />
+            </div>
             <NewsCards articles={newsArticles} />
         </div>
     )
