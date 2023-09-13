c951ad22580fc6881f86195960f09b6c57da8eaa
KitwanaSh <rodriguekitweze@gmail.com>
2023-09-13 14:11:18 +0000

    Feat: Add the scrolling function

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect, createRef } from 'react'
 import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from "@material-ui/core";
 import useStyles from './styles.js';
 import classNames from 'classnames';
 
 const NewsCard = ({ article : { description, publishedAt, source, title, url, urlToImage }, i, activeArticle }) => {
+  const [elRefs, setElRefs] = useState([]);
+  const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop - 50);
   const classes = useStyles();
 
+  useEffect(() => {
+    setElRefs((refs) => Array(20).fill().map((_, j) => refs[j] || createRef()));
+  }, [])
+
+  useEffect(() => {
+    if(i === activeArticle && elRefs[activeArticle]) {
+      scrollToRef(elRefs[activeArticle]);
+    }
+  }, [i, activeArticle, elRefs])
   return (
-      <Card className={classNames(classes.container, activeArticle === i ? classes.activeCard : null)}>
+      <Card ref={elRefs[i]} className={classNames(classes.container, activeArticle === i ? classes.activeCard : null)}>
         <CardActionArea href={url} target='_blank'>
             <CardMedia className={classes.media} image={urlToImage || 'https://buzzghana.com/wp-content/uploads/2013/09/news.jpg'} />
             <div className={classes.details}>
