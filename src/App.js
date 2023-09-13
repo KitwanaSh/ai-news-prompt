89f2b4b5e9bd03f2840e3d12a9e3c18e3b9c557d
KitwanaSh <rodriguekitweze@gmail.com>
2023-09-13 15:20:03 +0000

    Feat: Add the opening article

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react"
 import alanBtn from "@alan-ai/alan-sdk-web"
 import NewsCards from "./components/NewsCards/NewsCards";
+import wordsToNumbers from "words-to-numbers";
 
 import useStyles from "./styles.js"
 
@@ -14,12 +15,22 @@ const App = () => {
     useEffect(() => {
         alanBtn({
             key: alanKey,
-            onCommand: ({ command, articles }) => {
+            onCommand: ({ command, articles, number }) => {
                 if(command === 'newHeadlines') {
                     setNewsArticles(articles);
                     setActiveArticle(-1);
                 } else if(command === 'highlight') {
                     setActiveArticle((prevArticle) => prevArticle + 1);
+                } else if(command === 'open') {
+                    const parsedNumber = number.length > 2 ? wordsToNumbers(number, {fuzzy: true}) : number;
+                    const article = articles[parsedNumber - 1];
+
+                    if(parsedNumber > 20) {
+                        alanBtn().playText('Please, try that again.');
+                    } else if(article) {
+                        window.open(article.url, '_blank');
+                        alanBtn().playText('Opening...');
+                    }
                 }
             }
         })
